ee35cfaa17db41992fe27f8ec75fdf82af187ad6
Rory Schadler <48921090+roryschadler@users.noreply.github.com>
2025-11-26 18:22:05 -0500

    Julia setup

---
+++ src/advent_of_code_2025/utils/README.md
@@ -0,0 +1,3 @@
+# utils
+
+Create a file `session.txt` with your session token following the instructions at <https://github.com/wimglenn/advent-of-code-wim/issues/1>
---
+++ src/advent_of_code_2025/utils/utils.jl
@@ -0,0 +1,46 @@
+# This file contains utility functions for Advent of Code 2024.
+module Utils
+
+function readinput(path::String = "input.txt", T = String)
+    return open(f -> read(f, T), "$(dirname(PROGRAM_FILE))/$path")
+end
+
+function textblocktolines(textblock::AbstractString)::Vector{String}
+    return String.(filter(l -> length(l) > 0, split(textblock, "\n")))
+end
+
+function textblocktomatrix(textblock::AbstractString, T = Char; default = missing)
+    lines = textblocktolines(textblock)
+    m = length(lines[1])
+    n = length(lines)
+    @assert all(l -> length(l) == m, lines)
+
+    function local_parse(c)
+        if T <: Number
+            try
+                return parse(T, c)
+            catch
+                return default
+            end
+        else
+            return T(c)
+        end
+    end
+    mtx = Matrix{Union{T,Missing}}(undef, m, n)
+    for i in eachindex(lines)
+        for (j, c) in enumerate(lines[i])
+            mtx[j, i] = local_parse(c)
+        end
+    end
+    return mtx
+end
+
+function matrixtotextblock(mtx::Matrix{Char})::String
+    return join(transposelines([String(c) for c in eachrow(mtx)]), "\n")
+end
+
+function transposelines(lines::Vector{String})::Vector{String}
+    return [join([lines[i][j] for i = 1:length(lines)]) for j = 1:length(lines[1])]
+end
+
+end
